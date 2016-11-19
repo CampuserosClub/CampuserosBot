13f285db3d76c15e1d9eacd453762d918d02323c
João Vítor Dias <joaodias@noctus.org>
2016-11-19 01:26:15 -0200

    telegram-bot-sdk package

--- config/app.php
+++ config/app.php
@@ -167,7 +167,7 @@ return [
          * Package Service Providers...
          */
 
-        //
+        Telegram\Bot\Laravel\TelegramServiceProvider::class,
 
         /*
          * Application Service Providers...
