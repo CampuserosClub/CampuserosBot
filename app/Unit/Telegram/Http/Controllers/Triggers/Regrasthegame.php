c25d7aaf72c3ae432125be2e64b1f0af129709ac
João Paulo Polles <joaopaulopolles@gmail.com>
2016-12-12 08:33:49 -0200

    Corrigidas identações e etc no texto de regras

--- app/Unit/Telegram/Http/Controllers/Triggers/Regrasthegame.php
+++ app/Unit/Telegram/Http/Controllers/Triggers/Regrasthegame.php
@@ -16,15 +16,8 @@ class Regrasthegame extends Trigger
       $this->telegram->sendMessage([
           'chat_id' => $this->chat->getId(),
           'reply_to_message_id' => $this->message->getMessageId(),
-          'text' => "Olá! Se você solicitou essas regras, provavelmente não está jogando, mas alguém está, portanto: The Game! E alguém perdeu!\n\n
-          Há seis regras para jogar o O Jogo:\n
-          1) Não é possível ganhar 'O Jogo';\n
-          2) Todos que sabem do Jogo, automaticamente começam a jogar;\n
-          3) Sempre que alguém pensa no 'O Jogo', este alguém perde;\n
-          4) Cada vez que alguém perde 'O Jogo', deve ser anunciado a perda usando uma frase como 'eu perdi', 'perdi O Jogo', ou meios alternativos;\n
-          5) Deve ser respeitado o período de imunidade de no minimo 30 minutos depois de perder 'O jogo', durante o qual um jogador pode se lembrar do 'O jogo' sem perder novamente.\n
-          6) O jogador em imunidade esta proibido de se expressar verbalmente sobre o fato de ter perdido 'O jogo' até o fim de sua imunidade.\n
-          Lembrando que a expressão 'O Jogo' sempre se refere ao jogo e também \"The Game\".",
+          'text' => "
+Olá! Se você solicitou essas regras, provavelmente não está jogando, mas alguém está, portanto: *The Game*! E alguém perdeu!\n\nHá seis regras para jogar:\n1) Não é possível ganhar 'O Jogo';\n2) Todos que sabem do Jogo, automaticamente começam a jogar;\n3) Sempre que alguém pensa no 'O Jogo', este alguém perde;\n4) Cada vez que alguém perde 'O Jogo', deve ser anunciado a perda usando uma frase como 'eu perdi', 'perdi O Jogo', ou meios alternativos;\n5) Deve ser respeitado o período de imunidade de no minimo 30 minutos depois de perder 'O jogo', durante o qual um jogador pode se lembrar do 'O jogo' sem perder novamente.\n6) O jogador em imunidade esta proibido de se expressar verbalmente sobre o fato de ter perdido 'O jogo' até o fim de sua imunidade.\nLembrando que a expressão 'O Jogo' sempre se refere ao jogo e também a expressão 'The Game'.",
       ]);
     }
 }
