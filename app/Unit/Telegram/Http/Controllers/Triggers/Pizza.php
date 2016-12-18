b3153f583b44aa7770f914e4d84ab61c40b3f156
João Paulo Polles <joaopaulopolles@gmail.com>
2016-12-18 03:51:46 -0200

    Descomentado os itens de texto do trigger Pizza

--- app/Unit/Telegram/Http/Controllers/Triggers/Pizza.php
+++ app/Unit/Telegram/Http/Controllers/Triggers/Pizza.php
@@ -15,9 +15,9 @@ class Pizza extends Trigger
     ];
 
     protected $returns = [
-        // ['type' => 0, 'return' => 'Pizzaaaaaaaaaaaaaaaaaaaaaaaaaaaa'],
-        // ['type' => 0, 'return' => 'PIZZAAAAAAA'],
-        // ['type' => 0, 'return' => 'Pizzaaaaaaaaaaaaaaaaaaa!!!'],
+        ['type' => 0, 'return' => 'Pizzaaaaaaaaaaaaaaaaaaaaaaaaaaaa'],
+        ['type' => 0, 'return' => 'PIZZAAAAAAA'],
+        ['type' => 0, 'return' => 'Pizzaaaaaaaaaaaaaaaaaaa!!!'],
         ['type' => 2, 'return' => 'http://i.giphy.com/3o6ZtdOqypaPwQ5nMI.gif'],
     ];
 }
