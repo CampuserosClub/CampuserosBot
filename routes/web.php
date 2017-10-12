44b3e86a0ec12d217ca113c817892ef457788c21
João Vítor Dias <joaodias@noctus.org>
2017-10-12 19:39:40 -0300

    Remove default botman routes

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {return view('welcome');});
 
 Route::match(['get', 'post'], '/botman', 'BotManController@handle');
-Route::get('/botman/tinker', 'BotManController@tinker');
+//Route::get('/botman/tinker', 'BotManController@tinker');
