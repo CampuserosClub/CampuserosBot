d04db828846f151dccce0e304292c2b19d98435f
João Vítor Dias <joaodias@noctus.org>
2017-10-12 18:30:19 -0300

    About command

--- routes/botman.php
+++ routes/botman.php
@@ -1,9 +1,27 @@
 <?php
-use App\Http\Controllers\BotManController;
+
+use BotMan\BotMan\BotMan;
 
 $botman = resolve('botman');
 
-$botman->hears('Hi', function ($bot) {
-    $bot->reply('Hello!');
+//$payload = $bot->getMessage()->getPayload();
+//$replyTo = $payload['message_id'];
+
+// about
+
+$botman->hears('/about{whatever}', function (BotMan $bot) {
+    $user = $bot->getUser();
+    $username = $user->getUsername();
+    $name = $user->getFirstName();
+
+    $bot->typesAndWaits(3);
+
+    $sender = is_null($username)
+        ? $name
+        : "@" . $username;
+
+    $bot->reply("{$sender},\nEu sou um bot criado por campuseros, para campuseros 😁");
+    $bot->reply("Se você quiser saber como eu funciono, pode dar uma olhadinha no meu código:\nhttps://github.com/CampuserosClub/CampuserosBot", [
+        'disable_web_page_preview' => true,
+    ]);
 });
-$botman->hears('Start conversation', BotManController::class.'@startConversation');
